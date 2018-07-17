8edbbb038a8650f9a0dba8ce12caa34ec74eead0
Joel Aguilar <39573725+AguilarJoel@users.noreply.github.com>
2018-07-17 13:59:15 -0700

    Update DepositResponse.cpp

--- DepositResponse.cpp
+++ DepositResponse.cpp
@@ -1,4 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/**
+ * @file DepositResponse.cpp
+ *
+ * @brief
+ *    Getter and setter function definitions for deposit response attributes.
+ */
 
 #include "DepositResponse.h"
 
@@ -13,59 +18,109 @@ UDepositResponse::UDepositResponse()
 	// ...
 }
 
+/**
+ * @brief    Getter function for bank server
+ * @return   The bank server
+ */
 FString UDepositResponse::GetBankServer() const
 {
 	return bank_server;
 }
 
+/**
+ * @brief    Setter function for bank server
+ * @param    value The bank server
+ */
 void UDepositResponse::SetBankServer(const FString &value)
 {
 	bank_server = value;
 }
 
+/**
+ * @brief    Getter function for time
+ * @return   The deposit time
+ */
 FString UDepositResponse::GetTime() const
 {
 	return time;
 }
 
+/**
+ * @brief    Setter function for time
+ * @param    value The deposit time
+ */
 void UDepositResponse::SetTime(const FString &value)
 {
 	time = value;
 }
+
+/**
+ * @brief    Getter function for status
+ * @return   The deposit status
+ */
 FString UDepositResponse::GetStatus() const
 {
 	return status;
 }
 
+/**
+ * @brief    Setter function for status
+ * @param    value The deposit status
+ */
 void UDepositResponse::SetStatus(const FString &value)
 {
 	status = value;
 }
+
+/**
+ * @brief    Getter fucntion for message
+ * @return   The deposit message
+ */
 FString UDepositResponse::GetMessage() const
 {
 	return message;
 }
 
+/**
+ * @brief    Setter function for message
+ * @param    value The deposit message
+ */
 void UDepositResponse::SetMessage(const FString &value)
 {
 	message = value;
 }
 
+/**
+* @brief    Getter function for account
+* @return   The account
+*/
 FString UDepositResponse::GetAccount() const
 {
 	return account;
 }
 
+/**
+ * @brief    Setter function for account
+ * @param    value The account
+ */
 void UDepositResponse::SetAccount(const FString &value)
 {
 	account = value;
 }
 
+/**
+ * @brief    Getter function for receipt
+ * @return   The deposit receipt
+ */
 FString UDepositResponse::GetReceipt() const
 {
 	return receipt;
 }
 
+/**
+ * @brief    Setter function for receipt
+ * @param    value The deposit receipt
+ */
 void UDepositResponse::SetReceipt(const FString &value)
 {
 	receipt = value;
